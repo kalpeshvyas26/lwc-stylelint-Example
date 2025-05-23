d9fe84d11c500b5cb1dd1e6c8a13805030ebaac9
kalpeshvyas26 <kalpeshvyasprofessional@gmail.com>
2025-05-23 22:47:57 +0530

    Update .stylelintrc.js 7

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,39 +1,38 @@
 module.exports = {
-  
-  "extends": [
-    "stylelint-config-standard",
-    "stylelint-config-recommended-scss"
-  ],
 
-  "plugins": [
-    "stylelint-declaration-strict-value"
-  ],
+  "extends": [
+    "stylelint-config-standard",
+    "stylelint-config-recommended-scss"
+  ],
 
-  "rules": {
-    "declaration-no-important": true,
-    "scale-unlimited/declaration-strict-value": [
-      [
-        "color",
-        "background-color",
-        "border-color",
-        "margin",
-        "padding",
-        "font-size",
-        "line-height",
-        "font-weight"
-      ],
+  "plugins": [
+    "stylelint-declaration-strict-value"
+  ],
 
-      {
-        "ignoreValues": ["inherit", "transparent", "currentColor", "initial", "unset"],
-         // This regex aims to be more permissive with the content inside var() after the first comma.
+  "rules": {
+    "declaration-no-important": true,
+    "scale-unlimited/declaration-strict-value": [
+      [
+        "color",
+        "background-color",
+        "border-color",
+        "margin",
+        "padding",
+        "font-size",
+        "line-height",
+        "font-weight"
+      ],
+
+      {
+        "ignoreValues": ["inherit", "transparent", "currentColor", "initial", "unset"],
+        // This regex aims to be more permissive with the content inside var() after the first comma.
         "ignoreFunction": "/^var\\(--[a-zA-Z0-9-]+(?:,\\s*[^)]+)?\\)$/",
         "message": "Use design tokens or styling hooks instead of hardcoded values for ${property}.",
-        "ignoreCustomProperties": true 
-      }
-    ],
-    "color-no-hex": null,
-    "custom-property-pattern": null
-  }
+        "ignoreCustomProperties": true
+      }
+    ],
+    "color-no-hex": null,
+    "custom-property-pattern": null
+  }
 
 };
- 
